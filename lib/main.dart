5ef5d53c4c2354a4576e4fa0091934f112011e52
DaviSouza01 <davinsouza01@gmail.com>
2023-12-04 23:10:00 -0300

    Mudanças no Theme App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:strawhat_memory/pages/home_page.dart';
+import 'package:strawhat_memory/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Strawhat Memory',
       debugShowCheckedModeBanner: false,
-      theme: 
+      theme: StrawhatMemoryTheme.theme,
       home: const HomePage(),
     );
   }
